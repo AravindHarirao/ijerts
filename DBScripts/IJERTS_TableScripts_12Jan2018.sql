ea3cc7e42e7c67047b370bcb61afa771a3260e68
Aravind H <h.aravind@gmail.com>
2019-01-13 08:14:28 +0530

    specialisation changes- DB Script

---
+++ DBScripts/IJERTS_TableScripts_12Jan2018.sql
@@ -0,0 +1,47 @@
+CREATE TABLE `specialisation` (
+  `specialisationId` int(11) NOT NULL AUTO_INCREMENT,
+  `specialisation` varchar(100) DEFAULT NULL,
+  `IsActive` int(11) DEFAULT NULL,
+  PRIMARY KEY (`specialisationId`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+commit;
+
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Energy and combustion science (ECS)', 1)
+
+select * from `ijerts`.`specialisation`;
+commit;
+
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Energy and combustion science (ECS)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Thermodynamics of plasticity and fracture (TPF)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Fundamentals and applications of combustion science (FACS)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('New methods of measuring and/or correlating transport-property data', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Energy engineering (ENERG ENG)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Environmental applications of heat and/or mass transfer', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Manufacture of engineering components', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Micro manufacture (MM)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Heat and relevant mass transfer at all scales', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Drying Technology (DRY TECHNOL)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Micro Electro - Mechanical Systems (MEMS)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Advanced Fluid Mechanics (AFM)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Modern Tools and Mechanics (MTM) ', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Finite element analysis (FEA)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Engineering Materials', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Green Chemistry &amp; Technology (GCT)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Optimization Techniques', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Vacuum Science and Techniques (VST)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Refrigeration and Air Conditioning', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Computational Fluid Dynamics (CFD)', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Biomechanics and Biomechatronics', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Aerodynamics', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Composite Materials and Engineering', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Matrix Computation', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Photovoltaics &amp; Fuel Cell Technology', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Industrial Waste Treatment and Management', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Application of Probabilistic Methods in Engineering   ', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Structural Analysis, Thermodynamics and Thermo – Science', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Alternative energy conversion &amp; transport mechanisms', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('Communication Systems', 1);
+INSERT INTO `ijerts`.`specialisation` (specialisation, isactive) values ('VLSI Design', 1);
+commit;
+
